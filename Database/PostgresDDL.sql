19586f42dafe95d6f2075a6eee40a4d2e67eafcd
hanbyulkang <hanbyulkang@jinhak.com>
2020-11-05 16:49:36 +0900

    DB에서 Room Id관련 내용 제거

--- Database/PostgresDDL.sql
+++ Database/PostgresDDL.sql
@@ -1,20 +1,20 @@
-DROP TABLE IF EXISTS roomlist;
+DROP TABLE IF EXISTS roomlist cascade;
 CREATE TABLE roomlist (
   r_index SERIAL primary key,
-  roomId integer not null unique,
+  -- roomId integer not null unique,
   joinedMem varchar(2000) NOT NULL DEFAULT '{ }',
   roomName varchar(100) NOT NULL
 );
 
-DROP TABLE IF EXISTS chatcontentlist;
+DROP TABLE IF EXISTS chatcontentlist cascade;
 CREATE TABLE chatcontentlist (
   r_index SERIAL references roomlist(r_index),
-  roomId INTEGER NOT null unique references roomlist(roomid),
+  -- roomId INTEGER NOT null unique references roomlist(roomid),
   chatTime VARCHAR(50) NOT NULL,
   chatMsgList VARCHAR(20000) NOT NULL DEFAULT '{ }'
 );
 
-DROP TABLE IF EXISTS memberinfo;
+DROP TABLE IF EXISTS memberinfo cascade;
 CREATE TABLE memberinfo (
   m_index serial primary key,
   memID varchar(255) NOT null unique,
@@ -26,26 +26,26 @@ CREATE TABLE memberinfo (
   
 );
 
-DROP TABLE IF EXISTS chatlist;
+DROP TABLE IF EXISTS chatlist cascade;
 CREATE TABLE chatlist (
   m_index SERIAL primary key references memberinfo(m_index),
-  roomID varchar(500) NOT null unique,
+  -- roomID varchar(500) NOT null unique,
   chatList varchar(20000) NOT NULL DEFAULT '{ }'
 );
 
-DROP TABLE IF EXISTS friendlist;
+DROP TABLE IF EXISTS friendlist cascade;
 CREATE TABLE friendlist (
   m_index SERIAL primary key references memberinfo(m_index),
   memId varchar(255) NOT null unique references memberinfo(memid),
   fList varchar(20000) NOT NULL DEFAULT '{number: 0, list: { } }'  
 );
 
-DROP TABLE IF EXISTS userparticipatelist;
+DROP TABLE IF EXISTS userparticipatelist cascade;
 CREATE TABLE userparticipatelist (
   m_index serial primary key references memberinfo(m_index),
   userID varchar(50) NOT NULL,
   participateList varchar(20000) NOT NULL DEFAULT '{ }'
 );
 
-INSERT INTO "friendlist" VALUES (24,'jeta','{number: 0, list: { } }'),(26,'test','{number: 0, list: { } }');
 INSERT INTO "memberinfo" VALUES (24,'jeta','160efd3faadf4ec245d6313806e03deb335d2fde71ae93b2d89e54e9f1ac9025','jETA','null','{\"Alert\":{\"DesktopAlert\":true,\"MessagePreview\":false,\"AlertWithSound\":true},\"General\":{\"SendType\":0}}','\"{num: 0}\"'),(26,'test','293899b2e2eb8889e42fa0d13050f7b6df45d24f32d7acf36b9506a1517771ad','test','null','{\"Alert\":{\"DesktopAlert\":true,\"MessagePreview\":false,\"AlertWithSound\":true},\"General\":{\"SendType\":0}}','\"{num: 0}\"');
+INSERT INTO "friendlist" VALUES (24,'jeta','{number: 0, list: { } }'),(26,'test','{number: 0, list: { } }');
