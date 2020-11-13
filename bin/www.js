dd1e129c634eff2bb8e2af49fcda5569f5d30d99
hanbyulkang <hanbyulkang@jinhak.com>
2020-11-13 16:00:04 +0900

    기초 구조 제작 시작

--- bin/www.js
+++ bin/www.js
@@ -2,6 +2,7 @@
 var debug = require('debug')('my-application');
 var http = require('http');
 var app = require('../app');
+var VUE = require('vue');
 
 const port = 3000; //process.env.PORT || 8080;
 app.set('port', port);
@@ -12,6 +13,9 @@ server.listen(app.get('port'),function(){
     console.log("Server listening on port 8080.");
 });
 
+VUE = new Vue({
+    el: '#app'
+});
 
 // var server = app.listen(app.get('port'), function() {
 //   debug('Express server listening on port ' + server.address().port);
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+new Vue({
+    el: "#app",
+    // router,
+    // store,
+    render: h => h(App)
+});
